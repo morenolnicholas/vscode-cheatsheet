ec1e57182d7de545aae7fb67ffddf91a07534b2c
Nicholas Moreno <nicholaslonggeno.moreno@calbaptist.edu>
2020-12-18 13:24:57 -0800

    forgot to push original files done previously

---
+++ constants/shortcuts.js
@@ -0,0 +1,18 @@
+export const shortcuts = [
+    //General
+    { keybind: 'Ctrl+Shift+P, F1', action: 'Show Command Palette' },
+    { keybind: 'Ctrl+P', action: 'Quick Open, Go to File...' },
+    { keybind: 'Ctrl+Shift+N', action: 'New window/instance' },
+    { keybind: 'Ctrl+Shift+W', action: 'Close window/instance' },
+    { keybind: 'Ctrl+', action: 'User Settings' },
+    { keybind: 'Ctrl+K Ctrl+S', action: 'Keyboard Shortcuts' },
+    //Basic Editing
+    { keybind: 'Ctrl+X', action: 'Cut line (empty selection)' },
+    { keybind: 'Ctrl+C', action: 'Copy line (empty selection)' },
+    { keybind: 'Alt+ UP/DOWN', action: 'Move line up/down' },
+    { keybind: 'Shift+Alt+ UP/Down', action: 'Copy line up/down' },
+    { keybind: 'Ctrl+Shift+K', action: 'Delete line' },
+    { keybind: 'Ctrl+Enter', action: 'insert line below' },
+    { keybind: 'Ctrl+Shift+Enter', action: 'insert line above' },
+    { keybind: 'Ctrl+Shift+\'', action: 'Jump to matching bracket' },
+  ]
--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h2 class="text-xl font-semibold text-white 
+                      sm:text-2xl sm:leading-7 sm:text-black 
+                      md:text-3xl">
+          Visual Studio Code Cheat Sheet</h2>
+
+          <div>
+  <div class="mt-1 relative rounded-md shadow-sm">
+    <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+      
+    </div>
+    <input type="text" name="price" id="price" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="placeholdergoeshere"/>
+    <div class="absolute inset-y-0 right-0 flex items-center">
+      <label for="currency" class="sr-only">Currency</label>
+      <select id="Currency" name="currency" class="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md">
+        <option>Keybind</option>
+        <option>Action</option>
+      </select>
+    </div>
+  </div>
+</div>
     </div>
   );
 }
